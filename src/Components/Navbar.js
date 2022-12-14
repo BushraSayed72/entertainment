d4bda1f16a7087bccaf43ca59658bcd5a942c640
BushraSayed72 <sayedbushra72@gmail.com>
2022-12-14 16:38:56 +0530

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,54 +4,67 @@ import Features from './FeaturesList.js';
 import Company from './CompanyList.js';
 import down from '../images/icon-arrow-down.svg';
 import up from '../images/icon-arrow-up.svg';
-import FeaturesList from './FeaturesList.js';
-import CompanyList from './CompanyList.js';
-
-
+import { FaListUl } from 'react-icons/fa';
+import { FaCompressArrowsAlt } from 'react-icons/fa';
 function Navbar() {
 
   const [click, setClick]= useState (false);
   const [open, setOpen]= useState (false);
+  const handleClick =()=> setDropDownMobile(!dropDownMobile);
+  const closeMobileMenu =()=> setDropDownMobile (false);
+  const [dropDownMobile, setDropDownMobile]=useState (false);
   return (
     <div className='navbar'>
-    <div className='left-div'>
+     <div className='left-div'>
     <img src={snap} alt='snap'/>
-    <ul className='nav-list'>
+
+
+    <div className={dropDownMobile? 'nav-menu active': 'nav-menu'}>
+   
+    <ul className= 'nav-list'>
         <div className='features' >
-        <li>Features </li>
+        <li  className='nav-item' onClick={closeMobileMenu}>  Features </li>
         <div className='menu-icon' onClick={()=>{setClick(!click)}}>
         {click? <img src={down}  />: <img src ={up}/> }
         </div>
-        <FeaturesList 
+        <Features 
           click={click}/>
          
          </div> 
         <div className='features'>
-        <li>Company </li>
+        <li className='nav-item' onClick={closeMobileMenu}>Company </li>
         <div className='menu-icon' onClick={()=>{setOpen(!open)}} >
         {open? <img src={down}  />: <img src ={up}/> }
+
+
         </div>
          
-        <CompanyList 
+        <Company 
           open={open}
         />
         </div>
         
-        <li>Careers</li>
-        <li>About</li>
+        <li className='nav-item'  onClick={closeMobileMenu}>Careers</li>
+        <li className='nav-item'  onClick={closeMobileMenu}>About</li>
     </ul>
+    
+    </div>
     </div>
     <div className='right-div'>
-    <ul>
-        <li>Login</li>
-        </ul>
-
-        <button>Register</button>
-    </div>
-
+    <ul className='nav-list'>
+        <li className='nav-item' onClick={closeMobileMenu}>Login</li>
         
-      
+
+        <button className='nav-item' onClick={closeMobileMenu}>Register</button>
+        </ul>
+        
     </div>
+
+      
+      
+    <div className='menuu-icon' onClick={handleClick}> {dropDownMobile? <FaCompressArrowsAlt/>:<FaListUl/>  }</div>  
+  </div>
+
   )
 }
 
